00d5bed52297d5f6a54625cd67f5c8226992dc10
im_ay <132093979+icvf@users.noreply.github.com>
2024-05-21 20:09:22 +0100

    add the data to be shown

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unused_local_variable
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tawhida_login/pages/HomePage.dart';
@@ -7,7 +8,7 @@ import 'package:tawhida_login/pages/HomePage.dart';
 import 'login_or_register_page.dart';
 
 class AuthPage extends StatelessWidget {
-  const AuthPage({super.key});
+  AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +24,71 @@ class AuthPage extends StatelessWidget {
             );
           }
 
-          //user is not logged in
+          // user is not logged in
           else {
             return LoginOrRegisterPage();
           }
         },
       ),
+    );
+  }
+
+  // Declare the sensorData map
+  final Map<String, String> sensorData = {};
+
+  Widget sensorTile(String title, String userId, String field, String imagePath,
+      String unit) {
+    return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+      future: FirebaseFirestore.instance.collection('Users').doc(userId).get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData && snapshot.data!.data() != null) {
+            var data = snapshot.data!.data()!;
+            var value =
+                data[field] != null ? '${data[field]}$unit' : 'Unavailable';
+
+            // Store the sensor data in the map
+            sensorData[field] = data[field]?.toString() ?? 'Unavailable';
+
+            return Container(
+              margin: const EdgeInsets.only(bottom: 20),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ImageIcon(AssetImage(imagePath),
+                      size: 40, color: Colors.blue),
+                  const SizedBox(width: 18),
+                  Expanded(
+                    child: Text(
+                      '$title: $value',
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Text(
+              'Error: ${snapshot.error}',
+              style: const TextStyle(
+                color: Colors.red,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          }
+        }
+        return const CircularProgressIndicator();
+      },
     );
   }
 }
